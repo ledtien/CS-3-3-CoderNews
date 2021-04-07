190f7c4415645a8828a90ecec9d84befd36826b3
Tien <ldtien.1993@gmail.com>
2021-04-07 23:10:54 +0700

    add languages

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 // "https://newsapi.org/v2/top-headlines?country=us&cb7543222fe6414f8180cd191cc7a217=___OUR_API_KEY___"
 
-const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=cb7543222fe6414f8180cd191cc7a217";
+const url = "https://newsapi.org/v2/top-headlines?apiKey=cb7543222fe6414f8180cd191cc7a217";
+let userLanguage = "us";
 
 
 async function fetchData() {
-    const response = await fetch(url);
+    const response = await fetch(url + `&country=${userLanguage}`) ;
     const data = await response.json();
     renderArticles(data.articles)   
     console.log({data})  
@@ -16,7 +17,8 @@ async function fetchData() {
 function renderArticles(articles) {
     console.log({aSingleArticle: articles[0]})
     const articlesHTMLArray = articles.map(a => {
-        return ` <div class="card mb-5">
+        return ` <div class="card mb-5" style ="
+        box-shadow:20px 20px 10px grey; ">
         <img src="${a.urlToImage}" class="card-img-top" alt="...">
         <div class="card-body">
             <div style = "font-size: small; font-style: italic; display: flex; justify-content: space-between">
@@ -31,4 +33,14 @@ function renderArticles(articles) {
       </div>`
     })
      document.getElementById('new-list').innerHTML = articlesHTMLArray.join('');
+}
+
+function selectLanguage(language) {
+    userLanguage = language;
+    console.log({userLanguage})
+    fetchData(); 
+}
+
+function searchTitle(title) {
+    searchForTitle = title;
 }
